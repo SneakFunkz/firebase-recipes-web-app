40ac6168be3d66560a69a7a1a3a8de66c5655da5
NikitaGurbatov <93289908+SneakFunkz@users.noreply.github.com>
2022-02-08 13:54:44 +0000

    Added Create and Read operations for recipes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,158 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import FirebaseAuthService from "./FirebaseAuthService";
 import "./App.css";
 import LoginForm from "./components/LoginForm";
+import AddEditRecipeForm from "./components/AddEditRecipeForm";
+import FirebaseFirestoreService from "./FirebaseFirestoreService";
 
 function App() {
   const [user, setUser] = useState(null);
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetchRecipes()
+      .then((fetchedRecipes) => {
+        setRecipes(fetchedRecipes);
+      })
+      .catch((error) => {
+        console.lerror(error.message);
+        throw error;
+      });
+  }, [user]);
+
+  async function fetchRecipes() {
+    const queries = [];
+
+    //only published can be shown
+    if (!user) {
+      queries.push({
+        field: "isPublished",
+        condition: "==",
+        value: true,
+      });
+    }
+
+    let fetchedRecipes = [];
+
+    try {
+      //calls the read function on the 'recipes collection'
+      const response = await FirebaseFirestoreService.readDocuments({
+        collection: "recipes",
+        queries: queries,
+      });
+
+      //mreformats the data from the read call
+      const newRecipes = response.docs.map((recipeDoc) => {
+        const id = recipeDoc.id;
+
+        //the document info gets stored in the data variable
+        const data = recipeDoc.data();
+        data.publishDate = new Date(data.publishDate.seconds * 1000);
+
+        // returns an object with the data and the id of the document
+        return { ...data, id };
+      });
+
+      fetchedRecipes = [...newRecipes];
+    } catch (error) {
+      console.error(error.message);
+      throw error;
+    }
+
+    return fetchedRecipes;
+  }
+
+  async function handleFetchRecipes() {
+    try {
+      const fetchedRecipes = await fetchRecipes();
+
+      setRecipes(fetchedRecipes);
+    } catch (error) {
+      console.error(error.message);
+      throw error;
+    }
+  }
 
   FirebaseAuthService.subscribeToAuthChanges(setUser);
+
+  async function handleAddRecipe(newRecipe) {
+    try {
+      const response = await FirebaseFirestoreService.createDocument(
+        `recipes`,
+        newRecipe
+      );
+
+      handleFetchRecipes();
+
+      alert(`successfully created a recipe with an ID = ${response.id}`);
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  function lookupCategoryLabel(categoryKey) {
+    const categories = {
+      breadsSandwichesAndPizza: "Breads, Sandwiches and Pizza",
+      eggsAndBreakfast: "Eggs & Breakast",
+      desertsAndBakedGoods: "Deserts & Baked Goods",
+      fishAndSeafood: "Fish & Seafood",
+      vegetables: "Vegetables",
+    };
+
+    const label = categories[categoryKey];
+    return label;
+  }
+
+  function formatDate(date) {
+    const day = date.getUTCDate();
+    const month = date.getUTCMonth() + 1;
+    const year = date.getFullYear();
+    const dateString = `${day}-${month}-${year}`;
+
+    return dateString;
+  }
 
   return (
     <div className="App">
       <div className="title-row">
         <h1 className="title">Firebase Recipes</h1>
         <LoginForm existingUser={user}></LoginForm>
+      </div>
+      <div className="main">
+        <div className="center">
+          <div className="recipe-list-box">
+            {recipes && recipes.length > 0 ? (
+              <div className="recipe-list">
+                {/* Most common way to create a list in React */}
+                {/* This is what maps the data from the document to a list item in JSX */}
+                {recipes.map((recipe) => {
+                  return (
+                    <div className="recipe-card" key={recipe.id}>
+                      {recipe.isPublished === false ? (
+                        <div className="unpublished">UNPUBLISHED</div>
+                      ) : null}
+                      <div className="recipe-name">{recipe.name}</div>
+                      <div className="recipe-field">
+                        Category: {lookupCategoryLabel(recipe.category)}
+                      </div>
+                      <div className="recipe-field">
+                        Publish Date: {formatDate(recipe.publishDate)}
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            ) : null}
+          </div>
+        </div>
+
+        {/* If the user is set/logged in, the AddEditRecipeForm renders */}
+        {user ? (
+          <AddEditRecipeForm
+            handleAddRecipe={handleAddRecipe}
+          ></AddEditRecipeForm>
+        ) : null}
       </div>
     </div>
   );
